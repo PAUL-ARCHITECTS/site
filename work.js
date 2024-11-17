7de80971f34f7c5e86321bcad9c939cfb9380e28
PAUL-ARCHITECTS <54021273+PAUL-ARCHITECTS@users.noreply.github.com>
2024-11-17 22:25:52 +0800

    Update work.js

--- work.js
+++ work.js
@@ -89,8 +89,8 @@ function adjustImageSize(img) {
   const containerHeight = img.parentElement.clientHeight;
 
   // 计算缩放因子，确保图片适应容器且最大缩放倍数为3.5
-  const scaleFactorWidth = Math.min(containerWidth / originalWidth, 3.5);
-  const scaleFactorHeight = Math.min(containerHeight / originalHeight, 3.5);
+  const scaleFactorWidth = Math.min(containerWidth / originalWidth, 2);
+  const scaleFactorHeight = Math.min(containerHeight / originalHeight, 2);
 
   // 设置图片尺寸
   img.style.width = `${originalWidth * scaleFactorWidth}px`;
